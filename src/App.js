af692f0243293521428cce8d1e666fcddc291309
Christoph Steiner <gawen@t-online.de>
2020-05-04 17:57:54 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
+import Articles from "./Articles"
+
 
 function App() {
+
+
 	return (
+		<>
 		<div className='App'>
 			<header className='App-header'>HELLO</header>
 		</div>
+		<Articles />
+		</>
 	);
 }
 
