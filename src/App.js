7b94696313854d2a50e77bbdc1d173063d305b94
Christoph Steiner <gawen@t-online.de>
2020-05-05 08:24:44 +0200

    article implemted

--- src/App.js
+++ src/App.js
@@ -16,11 +16,29 @@ function App() {
 
 	return (
 		<>
-		<div className='App'>
-			{data != "" && <header className='App-header'>{data[0].title}</header>}
-			{console.log(data)}
-		</div>
-		<Articles />
+			<div className='App'>
+				{data != "" && <header className='App-header'>{data[0].title}</header>}
+				{console.log(data)}
+			</div>
+			<div className="grid_container">
+				<div className="article_grid">
+				{ [...data].map( article => {
+          return (
+            <Articles 
+				title={article.story_title} 
+				key={article.story_id} 
+					url={article.url} 
+					author={article.author} 
+					date={article.created_at}
+					tags={article.comment_text}
+					/>
+            )
+          }
+        )
+      }
+					<Articles />
+				</div>
+			</div>	
 		</>
 	);
 }
